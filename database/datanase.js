8c5a2605a32a5ab8d0cc19eb6e85bebdd71eca88
seif1000 <seifwin74@gmail.com>
2019-10-28 18:13:35 +0100

    last

--- database/datanase.js
+++ database/datanase.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 
-
+const key = require('../config/config');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'bootf',
-    password:'benmazouzseifeddine1994'
+    host:key.host,
+    user:key.user,
+    database:key.database,
+    password:key.password,
+    dialect:key.dialect
+    
 })
 
 module.exports = pool.promise();
